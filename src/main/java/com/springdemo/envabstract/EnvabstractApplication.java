dec2363c42d13354e8262ae0c7e62a61c53513ba
Hans <dbgtwd_klh@aliyun.com>
2019-10-30 00:32:45 +0800

    init

---
+++ src/main/java/com/springdemo/envabstract/EnvabstractApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.envabstract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvabstractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvabstractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/envabstract/EnvabstractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.envabstract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvabstractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
